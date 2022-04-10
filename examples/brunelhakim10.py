b7a58da92f7fd9a4a26d55a89beae83820681e11
Mario Kiefer <mario.kiefer@a1.net>
2022-04-10 17:02:31 +0200

    j= syntax for connect and split single SpikeMonitor results

--- examples/brunelhakim10.py
+++ examples/brunelhakim10.py
@@ -1,6 +1,7 @@
 
 from matplotlib.pyplot import figure, subplot, plot, xticks, ylabel, xlim, ylim, xlabel, clf
 from numpy import zeros, arange, ones
+import numpy as np
 import time
 start = time.time()
 #devicename = 'cuda_standalone'
@@ -114,44 +115,22 @@ for m in range(0, params['M']):
     lower = m * params['N']
     upper = (m+1) * params['N']
     subgroup = group[lower:upper]
-    conn.connect(condition='lower <= i and i < upper and lower <= j and j < upper ', p=sparseness)
+    # conn.connect(condition='lower <= i and i < upper and lower <= j and j < upper ', p=sparseness)
     subgroups.append(subgroup)
+
+# NEW better way
+param_N = params['N']
+conn.connect(j="k for k in sample(param_N*(i//param_N), param_N*(i//param_N+1),1, p=sparseness)")
 network.add(conn)
 
-
-# i_index = []
-# for m in range(0,M):
-#   for k in range(m*N,(m+1)*N):
-#     for i in range(0,N):
-#       i_index.__iadd__([k])
-# #print(i_index)
-#
-# print("FINISHED i_index")
-# j_index = []
-# for m in range(0,M):
-#   for k in range(m*N,(m+1)*N):
-#     for j in range(0,N):
-#       j_index.__iadd__([j+(m*N)])
-# #print(j_index)
-#
-# print("FINISHED j_index")
-#
-# NOT WORKING: only one iterator allowed
-#conn.connect(j='j+(m*N) for m in range(0,M) for k in range(m*N,(m+1)*N) for j in range(0,N)', skip_if_invalid=True)
-
-# if params['monitors']:
-#     M = SpikeMonitor(group)
-#     LFP = PopulationRateMonitor(group)
-
 if params['monitors']:
-    Ms = []
+    M = SpikeMonitor(group)
+    network.add(M)
     LFPs = []
+    # PopulationRateMonitor results can not be "untangled" after the simulation
     for subgroup in subgroups:
-        monitor = SpikeMonitor(subgroup)
         LFP = PopulationRateMonitor(subgroup)
-        network.add(monitor)
         network.add(LFP)
-        Ms.append(monitor)
         LFPs.append(LFP)
 
 
@@ -171,17 +150,30 @@ if params['profiling']:
 
 if params['monitors']:
 
-    for index, subgroup in enumerate(subgroups):
+    for m in range(0, params['M']):
+        lower = m * params['N']
+        upper = (m+1) * params['N']
+        subgroup = group[lower:upper]
+
         subplot(211)
-        plot(Ms[index].t/ms, Ms[index].i, '.')
+        points = np.c_[M.t/ms, M.i]
+        curPoints = points[np.where((lower <= points[:,1]) & (points[:,1] < upper))].T
+        # NOT WORKING.
+        # Not even deepcopy itself works, so I dont see generating multiple monitor objects after the fact happening.
+        # curM = deepcopy(M)
+
+        # This also fails on M. i is readonly., I think the monitor arrays can only be modified in the generated code
+        # curM.set_states()
+
+        plot(curPoints[0], curPoints[1], '.')
         xlim(0, duration/ms)
 
         subplot(212)
-        plot(LFPs[index].t/ms, LFPs[index].smooth_rate(window='flat', width=0.5*ms)/Hz)
+        plot(LFPs[m].t/ms, LFPs[m].smooth_rate(window='flat', width=0.5*ms)/Hz)
         xlim(0, duration/ms)
         #show()
 
-        plotpath = os.path.join(params['resultsfolder'], '{}.png'.format(name + "_Network_" + str(index+1)))
+        plotpath = os.path.join(params['resultsfolder'], '{}.png'.format(name + "_Network_" + str(m+1)))
         savefig(plotpath)
         print('plot saved in {}'.format(plotpath))
         clf()
